2c1557ecbe30bcd7d73969e84aee711cf0e68501
葛成杰 <30755703@qq.com>
2022-03-17 14:36:34 +0800

    feat: 增加一些类型的支持

--- lib/index.ts
+++ lib/index.ts
@@ -1,24 +1,27 @@
-class ThrottleFetch {
-    currentAction: () => Promise<any>
-    constructor(action: () => Promise<any>){
+class ThrottleFetch<T> {
+    currentAction: () => Promise<T>
+    constructor(action: () => Promise<T>)
+    {
         this.currentAction = action
     }
     // dirty 是用来节流请求的，flushing 是用来节流宏任务赋值操作的
     dirty = true
-    result = null
     flushing = false
-    set: Set<(value: unknown) => void> = new Set()
-    act = () => new Promise(async (resolve, reject) => {
+    do = false
+    result: T | null = null
+    set: Set<(value: ThrottleFetch<T>['result']) => void> = new Set()
+    act: () => Promise<ThrottleFetch<T>['result']> = () => new Promise(async (resolve: (value: ThrottleFetch<T>['result']) => void, reject) => {
         if(this.dirty) {
             try {
                 this.dirty = false
                 const res = await this.currentAction()
                 this.result = res
+                this.do = true
             } catch (error) {
                 reject(error)
             }
         }
-        if(this.result){
+        if(this.do){
             // 执行所有的异步任务
             resolve(this.result)
             // 执行所有的同步任务（每次微任务结束都会查询宏任务，节流一下）
@@ -43,7 +46,7 @@ class ThrottleFetch {
     refresh = async () => {
         if(!this.refreshing){
             this.refreshing = true
-            this.result = null
+            this.do = false
             this.flushing = false
             this.dirty = true
         }
